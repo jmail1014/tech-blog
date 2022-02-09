472ac6c9730a3ba6a680b3628c8e216b6d7abead
jclong <j.mail1014@gmail.com>
2022-02-09 16:42:13 -0500

    add set null on delete to post comment and user relationships

--- models/index.js
+++ models/index.js
@@ -6,34 +6,31 @@ const Comment = require("./Comment");
 // create associations
 User.hasMany(Post, {
   foreignKey: "user_id",
+  onDelete: 'SET NULL'
 });
 
 Post.belongsTo(User, {
   foreignKey: "user_id",
+  onDelete: 'SET NULL'
 });
-
-// User.belongsToMany(Post, {
-//   foreignKey: "user_id",
-// });
-
-// Post.belongsToMany(User, {
-//   foreignKey: "post_id",
-// });
 
 Comment.belongsTo(User, {
   foreignKey: "user_id",
+  onDelete: 'SET NULL'
 });
 
 Comment.belongsTo(Post, {
   foreignKey: "post_id",
+  onDelete: 'SET NULL'
 });
 
 User.hasMany(Comment, {
   foreignKey: "user_id",
+  onDelete: 'SET NULL'
 });
 
 Post.hasMany(Comment, {
-  foreignKey: "post_id",
+  foreignKey: "post_id"
 });
 
 module.exports = { User, Post, Comment };
